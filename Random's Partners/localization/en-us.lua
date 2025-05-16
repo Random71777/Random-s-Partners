0e790952c3e2226d87d9c2df8fbb35153f967a71
Random71777 <slydinodavid@gmail.com>
2025-05-15 20:32:25 -0700

    Version 0.1
    this is the very basic suit partners

---
+++ Random's Partners/localization/en-us.lua
@@ -0,0 +1,58 @@
+return {
+    descriptions = {
+               Partner={
+            pnr_RAND_lusty_joker={
+                name = "Infatuation",
+                text = {
+                   "Played cards with",
+                    "{C:hearts}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+            pnr_RAND_greedy_joker={
+                name = "Avarice",
+                text = {
+                   "Played cards with",
+                    "{C:diamonds}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+          pnr_RAND_wrathful_joker={
+                name = "war",
+                text = {
+                   "Played cards with",
+                    "{C:spades}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+           pnr_RAND_gluttenous_joker={
+                name = "hunger",
+                text = {
+                   "Played cards with",
+                    "{C:clubs}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+        }
+    },
+
+      misc = {
+        achievement_descriptions={},
+        achievement_names={},
+        blind_states={},
+        challenge_names={},
+        collabs={},
+        dictionary={},
+        high_scores={},
+        labels={},
+        poker_hand_descriptions={},
+        poker_hands={},
+        quips={},
+        ranks={},
+        suits_plural={},
+        suits_singular={},
+        tutorial={},
+        v_dictionary={},
+        v_text={},
+    },
+}
---
+++ Random's Partners/main.lua
@@ -0,0 +1,20 @@
+RAND = {}
+RAND.C = {}
+
+RAND.compat = {
+    partners = (SMODS.Mods['partner'] or {}).can_load or false
+}
+
+if RAND.compat.partners then
+SMODS.load_file("Items/partner.lua")()
+end
+
+if RAND.compat.partners then
+--Partner Mod atlas
+SMODS.Atlas{
+    key = "minijoke",
+    px = 46,
+    py = 58,
+    path = "partners.png"
+}
+end
