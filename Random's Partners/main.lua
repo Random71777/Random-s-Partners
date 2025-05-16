fb7bd8707c060cf20eea7a2b9a47c141e3edd6eb
Random71777 <slydinodavid@gmail.com>
2025-05-16 13:01:04 -0700

    Update main.lua

--- Random's Partners/main.lua
+++ Random's Partners/main.lua
@@ -2,14 +2,28 @@ RAND = {}
 RAND.C = {}
 
 RAND.compat = {
-    partners = (SMODS.Mods['partner'] or {}).can_load or false
+           partners = (SMODS.Mods['partner'] or {}).can_load or false,
+           paperback = (SMODS.Mods['paperback'] or {}).can_load or false
 }
 
 if RAND.compat.partners then
 SMODS.load_file("Items/partner.lua")()
 end
 
-if RAND.compat.partners then
+--Paperback Mod atlas
+SMODS.Atlas{
+    key = "minipaper",
+    px = 46,
+    py = 58,
+    path = "paperback.png"
+}
+
+
+if RAND.compat.paperback then
+SMODS.load_file("Items/Paperback.lua")()
+end
+
+
 --Partner Mod atlas
 SMODS.Atlas{
     key = "minijoke",
@@ -17,7 +31,7 @@ SMODS.Atlas{
     py = 58,
     path = "partners.png"
 }
-end
+
 
 SMODS.Atlas({
     key = "modicon",
