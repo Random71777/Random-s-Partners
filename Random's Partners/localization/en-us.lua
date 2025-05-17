a332f832d09a453d36ecbd8c908235f727d4461d
Random71777 <slydinodavid@gmail.com>
2025-05-17 15:01:13 -0700

    Update en-us.lua

--- Random's Partners/localization/en-us.lua
+++ Random's Partners/localization/en-us.lua
@@ -31,9 +31,9 @@ return {
                    "Played cards with",
                     "{C:clubs}#2#{} suit give",
                     "{C:mult}+#1#{} Mult when scored",
-                  },
-            },      
-                    pnr_RAND_fraudulent_joker={
+                },
+            },
+            pnr_RAND_fraudulent_joker={
                 name = "Scam",
                 text = {
                    "Played cards with",
@@ -46,6 +46,62 @@ return {
                 text = {
                    "Played cards with",
                     "{C:paperback_stars}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+            pnr_RAND_hedonist_joker={
+                name = "Euphoria",
+                text = {
+                   "Played cards with",
+                    "{C:minty_3s}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+           pnr_RAND_Envious_joker={
+                name = "Desire",
+                text = {
+                   "Played cards with",
+                    "{C:bunc_fleurons}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+            pnr_RAND_Proud_joker={
+                name = "Pride",
+                text = {
+                   "Played cards with",
+                    "{C:bunc_halberds}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+            pnr_RAND_envious_joker={
+                name = "Bitter",
+                text = {
+                   "Played cards with",
+                    "{C:six_stars}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+            pnr_RAND_slothful_joker={
+                name = "Coma",
+                text = {
+                   "Played cards with",
+                    "{C:six_moons}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+            pnr_RAND_gloomy_joker={
+                name = "Broken",
+                text = {
+                   "Played cards with",
+                    "{C:%s}#2#{} suit give",
+                    "{C:mult}+#1#{} Mult when scored",
+                },
+            },
+            pnr_RAND_cheerful_joker={
+                name = "Free",
+                text = {
+                   "Played cards with",
+                    "{C:purple}#2#{} suit give",
                     "{C:mult}+#1#{} Mult when scored",
                 },
             },
