6372ac14a04b90bb4740e748cffb923b3c8ce707
Random71777 <slydinodavid@gmail.com>
2025-05-17 15:02:08 -0700

    Update main.lua

--- Random's Partners/main.lua
+++ Random's Partners/main.lua
@@ -3,7 +3,11 @@ RAND.C = {}
 
 RAND.compat = {
            partners = (SMODS.Mods['partner'] or {}).can_load or false,
-           paperback = (SMODS.Mods['paperback'] or {}).can_load or false
+           paperback = (SMODS.Mods['paperback'] or {}).can_load or false,
+           MintysSillyMod = (SMODS.Mods['MintysSillyMod'] or {}).can_load or false,
+           Bunco = (SMODS.Mods['Bunco'] or {}).can_load or false,
+           SixSuits = (SMODS.Mods['SixSuits'] or {}).can_load or false,
+           InkAndColor = (SMODS.Mods['InkAndColor'] or {}).can_load or false
 }
 
 if RAND.compat.partners then
@@ -31,6 +35,56 @@ SMODS.Atlas{
     py = 58,
     path = "partners.png"
 }
+
+if RAND.compat.MintysSillyMod then
+SMODS.load_file("Items/Minty.lua")()
+end
+
+--MintysSillyMod Mod atlas
+SMODS.Atlas{
+    key = "miniminty",
+    px = 46,
+    py = 58,
+    path = "Minty.png"
+}
+
+if RAND.compat.Bunco then
+SMODS.load_file("Items/Bunco.lua")()
+end
+
+--Bunco Mod atlas
+SMODS.Atlas{
+    key = "minibunco",
+    px = 46,
+    py = 58,
+    path = "Bunco.png"
+}
+
+if RAND.compat.SixSuits then
+SMODS.load_file("Items/SixSuits.lua")()
+end
+
+--SixSuits Mod atlas
+SMODS.Atlas{
+    key = "minisix",
+    px = 46,
+    py = 58,
+    path = "SixSuits.png"
+}
+
+if RAND.compat.InkAndColor then
+SMODS.load_file("Items/InkAndColor.lua")()
+end
+
+--InkAndColor Mod atlas
+SMODS.Atlas{
+    key = "miniink",
+    px = 46,
+    py = 58,
+    path = "InkAndColor.png"
+}
+
+
 
 
 SMODS.Atlas({
